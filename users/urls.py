4ce847c010b7bc770429f0e7f941144cb2dca12a
fetehadin <fetehadinnegash@gmail.com>
2025-02-01 22:08:31 -0600

    Delete users directory

--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import user_list, user_create, user_delete
-
-urlpatterns = [
-    path('users/', user_list, name='user-list'),
-    path('users/create/', user_create, name='user-create'),
-    path('users/delete/<int:id>/', user_delete, name='user-delete'),
-]
